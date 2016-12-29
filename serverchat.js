d669c6fcb7c0aebddfcded516f6b8c8d5db233f3
xiongmingcai <1026911109@qq.com>
2016-12-29 16:30:37 +0800

    var  socket = io()

--- serverchat.js
+++ serverchat.js
@@ -1,5 +1,13 @@
-'use strict';
 
+var io = require('socket.io')();
 
+io.on('connection', function (socket) {
+    console.log('a user 用户上线...');
+    socket.on('disconnect', function () {
+        console.log('user 断开连接..');
+    });
+});
 
-
+exports.listen = function (server) {
+    io.listen(server)
+};

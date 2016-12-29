8124073bd75c2084e551bfef3c00f196a44907bd
xiongmingcai <1026911109@qq.com>
2016-12-29 18:39:41 +0800

    聊天室基础架构 上下线通知

--- serverchat.js
+++ serverchat.js
@@ -1,11 +1,13 @@
-
+//服务端
 var io = require('socket.io')();
 
 io.on('connection', function (socket) {
     console.log('a user 用户上线...');
     socket.on('disconnect', function () {
         console.log('user 断开连接..');
+        io.emit('offline','')
     });
+    io.emit('online','');//有人上线 向客户端发生 online 事件
 });
 
 exports.listen = function (server) {
